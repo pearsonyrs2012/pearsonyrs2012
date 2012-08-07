2b309ae456ffd973a7a26760147c4ae0ca50eb75
tycho <tycho@tychodevlaptop>
2012-08-07 14:11:55 +0100

    renamed public side static and data ingestion lib

---
+++ lib/main.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use Dancer;
+use Data::Dumper;
+get '/:lat,:long' => sub {
+my $filepath = '51.4971,-0.1382.13827'; #get file
+#fixmystreeturl: www.fixmystreet.com/rss/l/:lat,:long/:dist
+open(my $file, $filepath);
+my %categorys;
+while(<$file>) {
+	next unless /<category>([^<]*)<\/category>/;
+	my $category = $1;
+	$categorys{$category}++;
+}
+print Dumper \%categorys;
+}
