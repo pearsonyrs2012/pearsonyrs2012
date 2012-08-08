54ffafc620fcf83256a99fb144984a79a379acf4
tycho <tycho@tychodevlaptop>
2012-08-08 18:02:25 +0100

    test logging

--- lib/yrs2012.pm
+++ lib/yrs2012.pm
@@ -69,7 +69,7 @@ sub pizza {
 	my ($lat,$long) = @_;
 	my $url = 'https://maps.googleapis.com/maps/api/place/search/json?location='.$lat.','.$long.'&radius=2000&types=food&sensor=true&key=AIzaSyDdTCCT8WlzCIzqbmfWsTlWGZ6N5UFQ_Lg&keyword=pizza';
 	my $json = getfile($url);
-	return to_json $response if defined($response->{error});
+	return to_json $json if defined($json->{error});
 	warn "got json:$json->{content}";
 	my $hash = from_json($json->{content});
 	warn "got google hash";
