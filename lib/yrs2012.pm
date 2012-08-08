38018084bc20db6954bb37d4b653a5bd303a508a
tycho <tycho@tychodevlaptop>
2012-08-08 11:20:08 +0100

    routing changes

--- lib/yrs2012.pm
+++ lib/yrs2012.pm
@@ -3,14 +3,14 @@ use Dancer ':syntax';
 use LWP::UserAgent;
 our $VERSION = '0.1';
 
-get '/geo' => sub {
-	pass;
-};
 get '/' => sub {
+	send_file '/index.html';
+};
+get '/geo' => sub {
     send_file '/geo.html';
 };
 
-get '/api/:lat/:long/' => sub {
+get '/api/allcats/numbers/:lat/:long/' => sub {
 my $filepath = 'http://www.fixmystreet.com/rss/l/'.param('lat').','.param('long').'/2';#get file
 #fixmystreeturl: www.fixmystreet.com/rss/l/:lat,:long/:dist
 my $ua = LWP::UserAgent->new;
@@ -32,4 +32,28 @@ while(defined($line =shift(@lines))) {
 to_json \%categorys;
 
 };
+
+get '/api/allcats/levels/:lat/:long/' => sub {
+my $filepath = 'http://www.fixmystreet.com/rss/l/'.param('lat').','.param('long').'/2';#get file
+#fixmystreeturl: www.fixmystreet.com/rss/l/:lat,:long/:dist
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
+$ua->env_proxy;
+my $response = $ua->get($filepath);
+return "" if not $response->is_success;
+my $content = $response->content;
+return "invalid url $filepath" unless defined $content;
+my @lines = split("\n",$content);
+my %categorys;
+my $line;
+while(defined($line =shift(@lines))) {
+	next unless $line =~ /<category>([^<]*)<\/category>/;
+	my $category = $1;
+	$categorys{$category}++;
+}
+
+to_json \%categorys;
+
+};
+
 true;
