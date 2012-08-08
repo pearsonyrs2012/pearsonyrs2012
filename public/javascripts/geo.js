05b27a13f1a3c631ee8c79df8bd2094c2c816a13
tycho <tycho@tychodevlaptop>
2012-08-08 10:36:19 +0100

    geolocation updated

--- public/javascripts/geo.js
+++ public/javascripts/geo.js
@@ -7,26 +7,6 @@ function initiate_geolocation() {
 } 
 
 function handle_geolocation_query(position){  
-    //alert('Lat: ' + position.coords.latitude + ' ' +  
-      //    'Lon: ' + position.coords.longitude); 
-	  $('#coord').html("You are at latitude: " + position.coords.latitude + ' ' +  'Longitude: ' + position.coords.longitude)
-			drawmap(position.coords.latitude, position.coords.longitude)
+    alert('Lat: ' + position.coords.latitude + ' ' +  
+          'Lon: ' + position.coords.longitude);  
 }
-
-function drawmap(lat, long) {
-        var mapOptions = {
-          zoom: 16,
-          center: new google.maps.LatLng(lat, long),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(document.getElementById('map_canvas'),
-                                      mapOptions);
-
-        var image = 'images/map-pointer.png';
-        var myLatLng = new google.maps.LatLng(lat, long);
-        var beachMarker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            icon: image
-        });
-      }
