78540c70f54e7950eb0e810d5570a79f295512cc
Richard <richard@memespring.co.uk>
2012-08-08 15:01:03 +0100

    console javascrip extraction

--- public/javascripts/page2.js
+++ public/javascripts/page2.js
@@ -1,23 +1,28 @@
 jQuery(window).ready(function(){  
-    // jQuery("#btnInit").click(initiate_geolocation);
-initiate_geolocation()	
+  initiate_geolocation()	
 });  
 
 function initiate_geolocation() {  
     navigator.geolocation.getCurrentPosition(handle_geolocation_query);  
 } 
 
-function handle_geolocation_query(position){  
-    alert('Lat: ' + position.coords.latitude + ' ' +  
-          'Lon: ' + position.coords.longitude);
+function handle_geolocation_query(position){
 	$.ajax({
-		url: "api/overview/" + position.coords.latitude + "/" + position.coords.longitude + "/",
-		dataType: "json",
-		success: function(response) { console.log(response)		;
-		},
-		error: function(xhr,text,errort) {console.log(xhr);
-										console.log(text);
-										console.log(errort);
-											}
-	});
+      url: "api/overview/" + position.coords.latitude + "/" + position.coords.longitude + "/",
+      dataType: "json",
+      success: function(response) { 
+        console.log("success");
+        $(response).each(function(index) {
+            var level = $(this)[0]["level"]
+            var name = $(this)[0]["name"]
+            var pressentaionname = $(this)[0]["pressentaionname"]            
+            
+        });
+      },
+      error: function(xhr,text,errort) {console.log(xhr);
+              console.log("failed");
+              console.log(text);
+              console.log(errort);
+            }
+    });
 }
