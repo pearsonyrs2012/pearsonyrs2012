0ec19411b5be9723ed322575ee763dd79ad149b9
tycho <tycho@tychodevlaptop>
2012-08-10 17:27:20 +0100

    sound

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,11 +16,48 @@ jQuery(window).ready(function(){
 });  
 
 function initiate_geolocation() {  
-    navigator.geolocation.getCurrentPosition(handle_geolocation_query);  
+    navigator.geolocation.getCurrentPosition(handle_geolocation_query);
+
+	soundManager.setup({
+
+  // location: path to SWF files, as needed (SWF file name is appended later.)
+
+  url: 'javascripts/soundmanager/swf/',
+
+
+  // use soundmanager2-nodebug-jsmin.js, or disable debug mode (enabled by default) after development/testing
+  // debugMode: false,
+
+  // good to go: the onready() callback
+
+  onready: function() {
+
+    // SM2 has started - now you can create and play sounds!
+
+    //var mySound = soundManager.createSound({
+      //id: 'alert',
+      //url: '/path/'
+      // onload: function() { console.log('sound loaded!', this); }
+      // other options here..
+    //});
+
+    //mySound.play();
+
+  },
+
+  // optional: ontimeout() callback for handling start-up failure
+
+  ontimeout: function() {
+
+    // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+    // See the flashblock demo when you want to start getting fancy.
+
+  }
+	
 } 
 
 function handle_geolocation_query(position){
-  
+	
   	placename(position.coords.latitude, position.coords.longitude)
 
 	$.ajax({
