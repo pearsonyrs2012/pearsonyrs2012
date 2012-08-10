f39d59dc974ac49686237c22ddd2d270e46aefde
tycho <tycho@tychodevlaptop>
2012-08-10 09:50:29 +0100

    made index.js background colour change

--- public/javascripts/page2.js
+++ public/javascripts/page2.js
@@ -1,5 +1,18 @@
 jQuery(window).ready(function(){  
-  initiate_geolocation()	
+  initiate_geolocation()
+	$.ajax({
+      url: "api/settings/",
+      dataType: "json",
+      success: function(response) { 
+		//general
+		$('body').css('background-color','#' + response.backcolour)
+      },
+      error: function(xhr,text,errort) {console.log(xhr);
+              console.log("failed");
+              console.log(text);
+              console.log(errort);
+            }
+    });  
 });  
 
 function initiate_geolocation() {  
