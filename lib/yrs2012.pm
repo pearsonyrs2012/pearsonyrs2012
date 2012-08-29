3f8ff28d9deb62ca7d01f4cc2983a61f8dacbc00
tycho <tycho@hanoverdesktop>
2012-08-29 19:49:37 +0100

    fixing highlight bug

--- lib/yrs2012.pm
+++ lib/yrs2012.pm
@@ -6,6 +6,7 @@ use Template;
 our $VERSION = '0.1';
 
 set layout => 'main';
+set template => 'template_toolkit';
 
 get '/info' => sub {
 return to_json ({layout => setting('layout'), 'template engine' => setting('template') });
